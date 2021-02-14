c04f019603cb33219dba71304561cc8325d0bff0
zaprzemo <zaprzemo+github@gmail.com>
2021-02-14 11:41:55 +0100

    Added Pages home about -modified routing, -show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'pages/home', to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
   devise_for :users
   # get "/collaterals", to:"collateral#index"
   resources :collaterals
+  root to:'pages#home'
 end
